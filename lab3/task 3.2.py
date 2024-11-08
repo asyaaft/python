5883c977099044f04f6b7a705a7bdfc0568558d9
asyaaft <nastya17395@icloud.com>
2024-11-08 21:26:30 +0300

    Update task 3.2.py

--- lab3/task 3.2.py
+++ lab3/task 3.2.py
@@ -10,3 +10,4 @@ def find_common_participants(participants_first_group, participants_second_group
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 common_participants = find_common_participants(participants_first_group, participants_second_group)
+print(common_participants)
