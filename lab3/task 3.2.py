f56606ef7099f48a828d4c1ce6c64bc6bf3e6873
asyaaft <nastya17395@icloud.com>
2024-11-08 23:59:31 +0300

    Update task 3.2.py

--- lab3/task 3.2.py
+++ lab3/task 3.2.py
@@ -1,13 +1,12 @@
 # TODO Напишите функцию find_common_participants
-def find_common_participants(participants_first_group, participants_second_group, separator=","):
-    set1 = set(participants_first_group.split(separator))
-    set2 = set(participants_second_group.split(separator))
-    common_participants = list(set1.intersection(set2))
-    common_participants.sort()
-    return common_participants
+def find_common_participants(participants1, participants2, separator=",") -> object:
+    participants_first_group = set(participants1.split(separator))
+    participants_second_group = set(participants2.split(separator))
+    common_participants = participants_first_group.intersection(participants_second_group)
+    return sorted(common_participants)
 
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-common_participants = find_common_participants(participants_first_group, participants_second_group)
-print(find_common_participants(participants_first_group, participants_second_group), common_participants)
+common_participants = find_common_participants(participants_first_group, participants_second_group, separator="|")
+print(common_participants)
